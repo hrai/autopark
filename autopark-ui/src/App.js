28e331ad7ab577d0c2744327e094db07b53b590f
Hangjit Rai <hangjit.rai@outlook.com>
2020-05-04 21:45:58 +1000

    added home and routing

--- autopark-ui/src/App.js
+++ autopark-ui/src/App.js
@@ -1,26 +1,58 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Visits from './Visits';
+import Home from './Home';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div>
+        <nav>
+          <ul>
+            <li>
+              <Link to="/">Home</Link>
+            </li>
+            <li>
+              <Link to="/visits">Visits</Link>
+            </li>
+          </ul>
+          <Switch>
+            <Route path="/visits">
+              <Visits />
+            </Route>
+            <Route path="/">
+              <Home />
+            </Route>
+          </Switch>
+        </nav>
+      </div>
+    </Router>
   );
+  // return (
+  //   <div className="App">
+  //     <header className="App-header">
+  //       <img src={logo} className="App-logo" alt="logo" />
+  //       <p>
+  //         Edit <code>src/App.js</code> and save to reload.
+  //       </p>
+  //       <a
+  //         className="App-link"
+  //         href="https://reactjs.org"
+  //         target="_blank"
+  //         rel="noopener noreferrer"
+  //       >
+  //         Learn React
+  //       </a>
+  //     </header>
+  //   </div>
+  // );
 }
 
 export default App;
---
+++ autopark-ui/src/Home.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+
+export default function Home() {
+  return (
+    <div>Testing new home</div>
+  );
+}
