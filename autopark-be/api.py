12648436bd4c3328bda195efe3d12c140a1552e3
Patrick-Sabry <patricksabry97@hotmail.com>
2020-05-23 12:44:47 +1000

    fixed be API

--- autopark-be/api.py
+++ autopark-be/api.py
@@ -1,22 +1,24 @@
 import pandas as pd 
 import numpy as np 
-import tensorflow
 
-from flask import Flask
+from flask import Flask, json_available, jsonify
 
 app = Flask(__name__)
+app.config["DEBUG"] = True
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def hello_world():
-    return 'Hello, World!'
-
+    response = { 'Status': "API is up and running!" }
+    return jsonify(response),200
 
 #TODO - data pre-processing function
 def preprocess():
     print("1")
 
-
 #TODO - Model prediction function    
 @app.route('/predict')
 def predict():
     print("2")
+
+
+app.run()
