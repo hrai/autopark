ad8bacde7f8096153a21028bf5cc31bb1a95ca06
Hangjit Rai <hangjit.rai@outlook.com>
2020-05-23 17:59:13 +1000

    Enabled Cors

--- autopark-be/api.py
+++ autopark-be/api.py
@@ -1,12 +1,16 @@
-import pandas as pd 
-import numpy as np 
+import pandas as pd
+import numpy as np
 
 from flask import Flask, json_available, jsonify
+from flask_cors import CORS, cross_origin
 
 app = Flask(__name__)
+cors= CORS(app)
 app.config["DEBUG"] = True
+app.config["CORS_HEADERS"] = 'Content-Type'
 
 @app.route('/', methods=['GET'])
+@cross_origin()
 def hello_world():
     response = { 'Status': "API is up and running!" }
     return jsonify(response),200
@@ -15,10 +19,12 @@ def hello_world():
 def preprocess():
     print("1")
 
-#TODO - Model prediction function    
-@app.route('/predict')
-def predict():
-    print("2")
+#TODO - Model prediction function
+@app.route('/predict', methods=['POST'])
+@cross_origin()
+def predict(data):
+    # print(data)
+    return 'predicted',200
 
 
 app.run()
