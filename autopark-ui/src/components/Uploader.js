73f6f3fdb3461952c0cbc8d0f4743a5c7e5cd4f3
Hangjit Rai <hangjit.rai@outlook.com>
2020-06-04 14:56:14 +1000

    Removed React Form

--- autopark-ui/src/components/Uploader.js
+++ autopark-ui/src/components/Uploader.js
@@ -1,23 +1,22 @@
 import React from 'react';
-import {useForm} from 'react-hook-form';
 import axios from 'axios';
 import {withRouter} from "react-router";
 
 function UploaderComp(props) {
-  const {handleSubmit} = useForm();
-  const onSubmit = values => postImage(values);
+  const onSubmit = (event) => {
+    event.preventDefault();
+    postImage(event.target)
+  };
 
-  function postImage(imageData) {
+  function postImage(form) {
     const url = 'http://localhost:5000/predict';
 
-    var bodyFormData = new FormData();
-    bodyFormData.set('filename', 'TestFile');
-    bodyFormData.append('image', imageData);
+    var formData = new FormData(form);
 
-    axios.post(url, bodyFormData, {
+    axios.post(url, formData, {
       headers: {
         'Access-Control-Allow-Origin': '*',
-        'Content-Type': 'multipart/form-data',
+        'Content-Type': 'application/x-www-form-urlencoded'
       }
     })
       .then(response => {
@@ -29,14 +28,14 @@ function UploaderComp(props) {
 
   return (
     <div class="container">
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={onSubmit}>
         <div class="form-group">
-          <label for="uploader">Image</label>
-          <input type="file" class="form-control" id="uploader" aria-describedby="emailHelp" />
+          <label for="num_plate">Image</label>
+          <input type="file" class="form-control" name="num_plate" id="num_plate" aria-describedby="emailHelp" />
         </div>
         <button type="submit" class="btn btn-primary">Submit</button>
       </form>
-    </div>
+    </div >
   );
 }
 
